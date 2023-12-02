b06238d73fe603be6d4ffe5ab73f1baaa104ea33
ibrahimkebapci <ibrahim_kebapci@hotmail.com>
2023-12-02 23:53:57 +0300

    Update

---
+++ JavaScript/Day-10/js/script.js
@@ -0,0 +1,18 @@
+import { countries } from "../data/countries.js";
+
+const loadCountries = () =>
+{
+    let strCountry = "";
+
+    countries.forEach ((item) =>
+    {
+        strCountry +=  <option>Angola</option>
+    })
+
+    const lstCountries = document.querySelector("#lstCountries");
+
+    lstCountries.innerHTML = strCountry;
+}
+
+
+loadCountries(); // dakika 47 
