bd8d6a13d276481d851fe411424d51f8072b6eee
ibrahimkebapci <ibrahim_kebapci@hotmail.com>
2024-01-17 10:18:58 +0300

    Update

---
+++ CypressB249/cypress/e2e/3-lessons/Day01/01-ilkTestim.cy.js
@@ -0,0 +1,29 @@
+describe('ilk Testim', () => 
+{
+    it('true, true ile eşit mi!', () =>
+    {
+        expect(true).to.equal(true)
+    })
+
+    it('true, true ile eşit mi!', () =>
+    {
+       //Intellisense
+       cy.get('').click();
+       cy.get('').find('');
+    })
+})
+
+
+describe('İlk Testim', () => {
+    it('true, true ile eşit mi!' , () => {
+    expect(true).to.equal(true)
+    // 1. Kullanıcı URL ' gider
+    // 2. Login butonuna tıklanır
+    // 3. Geçerli bir user name girelir
+    // 4. GEçerli bir şifre girer
+    // 5. Login butonuna tıklar
+    // 6. Login işleminin başarılı olduğu doğrulanır
+    })
+    
+
+   })
