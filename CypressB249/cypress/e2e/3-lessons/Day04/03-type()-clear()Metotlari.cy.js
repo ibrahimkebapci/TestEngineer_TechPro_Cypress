fa6bda362408b360dc264b55e970788b0d8ff4b3
ibrahimkebapci <ibrahim_kebapci@hotmail.com>
2024-01-29 19:46:37 +0300

    Update

---
+++ CypressB249/cypress/e2e/3-lessons/Day04/03-type()-clear()Metotlari.cy.js
@@ -0,0 +1,17 @@
+describe('Eylem Metodlari-type() ve clear() metodlari')
+
+    it('type() veclear()metod kullanimi', ()=>
+    {
+        cy.visit('https://www.letskodeit.com/practice');
+
+        // cy.get('#disable-button').click()
+        // cy.get('#enable-example-input').type('Techpro',{force: true})
+
+        cy.get('#enable-example-input').type('asdaf{selectAll}{bakspace}',{delay:1000});
+
+        it('Kitap Yurdu Arama Motorunda Sequemce Kullanimi', ()=>
+        {
+            cy.visit('https://www.kitapyurud.com/');
+            cy.get('#search-input').type('Cypress{Enter}');
+        })
+    })
